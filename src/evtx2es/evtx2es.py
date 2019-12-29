bf54daad6774eb03e07ed5cfb218910e0e9f8f24
Thomas Patzke <thomas@patzke.org>
2019-12-29 01:40:00 +0100

    Conversion to ECS format

--- src/evtx2es/evtx2es.py
+++ src/evtx2es/evtx2es.py
@@ -48,6 +48,64 @@ class Evtx2es(object):
             except Exception:
                 pass
 
+            # Convert data according to ECS (sort of)
+            # First copy system fields
+            record['winlog'] = {
+                    'channel': record['data']['Event']['System']['Channel'],
+                    'computer_name': record['data']['Event']['System']['Computer'],
+                    'event_id': record['data']['Event']['System']['EventID'],
+                    'opcode': record['data']['Event']['System'].get('Opcode'),
+                    'provider_guid': record['data']['Event']['System']['Provider']['#attributes'].get('Guid'),
+                    'provider_name': record['data']['Event']['System']['Provider']['#attributes']['Name'],
+                    'record_id': record['data']['Event']['System']['EventRecordID'],
+                    'task': record['data']['Event']['System']['Task'],
+                    'version': record['data']['Event']['System'].get('Version'),
+                    }
+            try:
+                record['winlog']['process'] = {
+                        'pid': record['data']['Event']['System']['Execution']['#attributes']['ProcessID'],
+                        'thread_id': record['data']['Event']['System']['Execution']['#attributes']['ThreadID'],
+                        }
+            except KeyError:
+                pass
+
+            record.update({
+                'log': {
+                    'file': {
+                            'name': str(self.path)
+                        }
+                    },
+                'event': {
+                    'code': record['winlog']['event_id'],
+                    'created': record['data']['Event']['System']['TimeCreated']['#attributes']['SystemTime'],
+                    }
+                })
+            record['@timestamp'] = record['event']['created']
+
+            # Move event attributes to ECS location
+            record['winlog']['event_data'] = record['data']['Event'].get('EventData', dict())
+            del record['data']
+            if record['winlog']['event_data'] is None or len(record['winlog']['event_data']) == 0:    # remove event_data fields if empty
+                del record['winlog']['event_data']
+            else:
+                for k, v in record['winlog']['event_data'].items():
+                    # Normalize some known problematic fields with values switching between integers and strings with hexadecimal notation to integers
+                    if k in ('ProcessId') and type(v) == str:
+                        if v.startswith("0x"):
+                            record['winlog']['event_data'][k] = int(v, 16)
+                        else:
+                            try:
+                                record['winlog']['event_data'][k] = int(v)
+                            except ValueError:
+                                record['winlog']['event_data'][k] = 0
+
+                    # Maximum limit of numeric values in Elasticsearch
+                    if type(v) is int:
+                        if v < -2 ** 63:
+                            record['winlog']['event_data'][k] = -2 ** 63
+                        elif v > 2 ** 63 - 1:
+                            record['winlog']['event_data'][k] = 2 ** 63 - 1
+
             buffer.append(record)
 
             if len(buffer) >= size:
