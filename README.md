c14b793effa8f717c211ab601cf58cb3084fbb3c
sumeshi <j15322sn@gmail.com>
2020-06-12 03:49:46 +0900

    feat: hash-id, docstring, split funcs

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # Evtx2es
+
 [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
 
-Import Windows EventLogs(.evtx files) to ElasticSearch.
+Fast import of Windows EventLogs(.evtx) into Elasticsearch.
 
-Life is too short and there is not enough time to process huge Windows EventLogs with pure-Python software.  
+Life is too short and there is not enough time to process **huge Windows EventLogs** with **pure-Python software**.  
 evtx2es uses Rust library [pyevtx-rs](https://github.com/omerbenamram/pyevtx-rs), so it runs much faster than traditional software.
 
-```
-Note: Nov 11, 2019
-    Moved main development location to gitlab
-```
-
 ## Usage
+
 ```bash
 $ evtx2es /path/to/your/file.evtx
 ```
@@ -19,7 +16,7 @@ $ evtx2es /path/to/your/file.evtx
 or
 
 ```python
-from evtx2es.evtx2es import evtx2es
+from evtx2es import evtx2es
 
 if __name__ == '__main__':
     filepath = '/path/to/your/file.evtx'
@@ -29,11 +26,12 @@ if __name__ == '__main__':
 ### Args
 
 evtx2es supports multiple file input, all arguments are determined as file paths.
+
 ```bash
 $ evtx2es file1.evtx file2.evtx file3.evtx
 ```
 
-or 
+or
 
 ```bash
 $ tree .
@@ -51,41 +49,44 @@ $ evtx2es /evtxfiles/ # The Path is recursively expanded to file1~6.evtx.
 ```
 
 ### Options
+
 ```
---host: 
-    ElasticSearch host address
-    (default: localhost)
+--host:
+  ElasticSearch host address
+  (default: localhost)
 
---port: 
-    ElasticSearch port number
-    (default: 9200)
+--port:
+  ElasticSearch port number
+  (default: 9200)
 
---index: 
-    Index name
-    (default: evtx2es)
-
---type: 
-    Document-type name
-    (default: evtx2es)
+--index:
+  Index name
+  (default: evtx2es)
 
 --size:
-    bulk insert size
-    (default: 500)
+  bulk insert size
+  (default: 500)
+
+--scheme:
+  Scheme to use (http, or https)
+  (default: http)
 ```
 
 ### Examples
+
 ```
-$ evtx2es /path/to/your/file.evtx --host=localhost --port=9200 --index=foo --type=bar --size=500
+$ evtx2es /path/to/your/file.evtx --host=localhost --port=9200 --index=foo --size=500
 ```
 
 ```py
 if __name__ == '__main__':
-    evtx2es('/path/to/your/file.evtx', host=localhost, port=9200, index='foo', type='bar', size=500)
+    evtx2es('/path/to/your/file.evtx', host=localhost, port=9200, index='foo', size=500)
 ```
 
 ## Output Format
 
 Using the sample evtx file of [JPCERT/CC:LogonTracer](https://github.com/JPCERTCC/LogonTracer) as an example.
+
 ```
 [
   {
@@ -145,6 +146,7 @@ Using the sample evtx file of [JPCERT/CC:LogonTracer](https://github.com/JPCERTC
 ```
 
 ## Performance Evaluations
+
 evtx2es was evaluated using the sample evtx file of [JPCERT/CC:LogonTracer](https://github.com/JPCERTCC/LogonTracer) (about 30MB binary data).
 
 ```.bash
@@ -155,17 +157,20 @@ $ time evtx2es ./Security.evtx
 See [Qiita](https://qiita.com/sumeshi/items/cb2fbafe59c2c83e3085) for more information.
 
 ### Running Environment
+
 ```
-OS: Ubuntu 18.04  
-CPU: Intel Core i5-6500  
-RAM: DDR4 32GB  
+OS: Ubuntu 18.04
+CPU: Intel Core i5-6500
+RAM: DDR4 32GB
 ```
 
 ElasticSearch 7.4 was running on the Docker version(Official Image).  
 https://hub.docker.com/_/elasticsearch
 
 ## Installation
+
 ### via pip
+
 ```
 $ pip install git+https://github.com/sumeshi/evtx2es
 ```
@@ -175,6 +180,7 @@ The source code for evtx2es is hosted at GitHub, and you may download, fork, and
 Please report issues and feature requests. :sushi: :sushi: :sushi:
 
 ## License
+
 evtx2es is released under the [MIT](https://github.com/sumeshi/evtx2es/blob/master/LICENSE) License.
 
 Powered by [pyevtx-rs](https://github.com/omerbenamram/pyevtx-rs).  
--- src/evtx2es.py
+++ src/evtx2es.py
@@ -3,9 +3,9 @@
 import json
 import argparse
 import traceback
-
 from pathlib import Path
 from typing import List, Generator
+from hashlib import sha1
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
@@ -15,14 +15,31 @@ from tqdm import tqdm
 
 
 class ElasticsearchUtils(object):
-    def __init__(self, hostname: str, port: int, scheme: str) -> None:
+    def __init__(self, hostname: str, port: int, scheme: str):
         self.es = Elasticsearch(host=hostname, port=port, scheme=scheme)
 
-    def bulk_indice(self, records, index_name: str, type_name: str) -> None:
+    def calc_hash(self, record: dict) -> str:
+        """Calculate hash value from record.
+
+        Args:
+            record (dict): Eventlog record.
+
+        Returns:
+            str: Hash value
+        """
+        return sha1(json.dumps(record, sort_keys=True).encode()).hexdigest()
+
+    def bulk_indice(self, records: List[dict], index_name: str) -> None:
+        """Bulk indices the documents into Elasticsearch.
+
+        Args:
+            records (List[dict]): List of each records read from evtx files.
+            index_name (str): Target Elasticsearch Index.
+        """
         bulk(
             self.es,
             [
-                {"_index": index_name, "_type": type_name, "_source": record}
+                {"_id": self.calc_hash(record), "_index": index_name, "_source": record}
                 for record in records
             ],
             raise_on_error=False,
@@ -30,99 +47,121 @@ class ElasticsearchUtils(object):
 
 
 class Evtx2es(object):
-    def __init__(self, filepath: str) -> None:
-        self.path = Path(filepath)
+    def __init__(self, filepath: str):
+        self.path = Path(filepath).resolve()
         self.parser = PyEvtxParser(self.path.open(mode="rb"))
 
-    def gen_json(self, size: int) -> Generator:
-        buffer: List[dict] = []
-        for record in self.parser.records_json():
-            record["data"] = json.loads(record.get("data"))
+    def format_record(self, record: dict) -> dict:
+        record["data"] = json.loads(record.get("data", "{}"))
 
-            eventid_field = record.get("data").get("Event").get("System").get("EventID")
-            if type(eventid_field) is dict:
-                record["data"]["Event"]["System"]["EventID"] = eventid_field.get(
-                    "#text"
-                )
+        eventid_field = (
+            record.get("data", dict())
+            .get("Event", dict())
+            .get("System", dict())
+            .get("EventID")
+        )
+        if type(eventid_field) is dict:
+            record["data"]["Event"]["System"]["EventID"] = eventid_field.get("#text")
 
-            try:
-                status = record.get("data").get("Event").get("EventData").get("Status")
-                record["data"]["Event"]["EventData"]["Status"] = None
-            except Exception:
-                pass
+        status = (
+            record.get("data", dict())
+            .get("Event", dict())
+            .get("EventData", dict())
+            .get("Status")
+        )
+        if status:
+            record["data"]["Event"]["EventData"]["Status"] = status
 
-            # Convert data according to ECS (sort of)
-            # First copy system fields
-            record["winlog"] = {
-                "channel": record["data"]["Event"]["System"]["Channel"],
-                "computer_name": record["data"]["Event"]["System"]["Computer"],
-                "event_id": record["data"]["Event"]["System"]["EventID"],
-                "opcode": record["data"]["Event"]["System"].get("Opcode"),
-                "provider_guid": record["data"]["Event"]["System"]["Provider"][
+        # Convert data according to ECS (sort of)
+        # First copy system fields
+        record["winlog"] = {
+            "channel": record["data"]["Event"]["System"]["Channel"],
+            "computer_name": record["data"]["Event"]["System"]["Computer"],
+            "event_id": record["data"]["Event"]["System"]["EventID"],
+            "opcode": record["data"]["Event"]["System"].get("Opcode", ""),
+            "provider_guid": record["data"]["Event"]["System"]["Provider"][
+                "#attributes"
+            ].get("Guid", ""),
+            "provider_name": record["data"]["Event"]["System"]["Provider"][
+                "#attributes"
+            ]["Name"],
+            "record_id": record["data"]["Event"]["System"]["EventRecordID"],
+            "task": record["data"]["Event"]["System"]["Task"],
+            "version": record["data"]["Event"]["System"].get("Version", ""),
+        }
+
+        try:
+            record["winlog"]["process"] = {
+                "pid": record["data"]["Event"]["System"]["Execution"]["#attributes"][
+                    "ProcessID"
+                ],
+                "thread_id": record["data"]["Event"]["System"]["Execution"][
                     "#attributes"
-                ].get("Guid"),
-                "provider_name": record["data"]["Event"]["System"]["Provider"][
-                    "#attributes"
-                ]["Name"],
-                "record_id": record["data"]["Event"]["System"]["EventRecordID"],
-                "task": record["data"]["Event"]["System"]["Task"],
-                "version": record["data"]["Event"]["System"].get("Version"),
+                ]["ThreadID"],
             }
-            try:
-                record["winlog"]["process"] = {
-                    "pid": record["data"]["Event"]["System"]["Execution"][
+        except KeyError:
+            pass
+
+        record.update(
+            {
+                "log": {"file": {"name": self.path.name}},
+                "event": {
+                    "code": record["winlog"]["event_id"],
+                    "created": record["data"]["Event"]["System"]["TimeCreated"][
                         "#attributes"
-                    ]["ProcessID"],
-                    "thread_id": record["data"]["Event"]["System"]["Execution"][
-                        "#attributes"
-                    ]["ThreadID"],
-                }
-            except KeyError:
-                pass
+                    ]["SystemTime"],
+                },
+            }
+        )
+        record["@timestamp"] = record["event"]["created"]
 
-            record.update(
-                {
-                    "log": {"file": {"name": str(self.path)}},
-                    "event": {
-                        "code": record["winlog"]["event_id"],
-                        "created": record["data"]["Event"]["System"]["TimeCreated"][
-                            "#attributes"
-                        ]["SystemTime"],
-                    },
-                }
-            )
-            record["@timestamp"] = record["event"]["created"]
+        # Move event attributes to ECS location
+        record["winlog"]["event_data"] = record["data"]["Event"].get(
+            "EventData", dict()
+        )
+        del record["data"]
+        if (
+            record["winlog"]["event_data"] or len(record["winlog"]["event_data"]) == 0
+        ):  # remove event_data fields if empty
+            del record["winlog"]["event_data"]
+        else:
+            for k, v in record["winlog"]["event_data"].items():
+                # Normalize some known problematic fields with values switching between integers and strings with hexadecimal notation to integers
+                if k in ("ProcessId") and type(v) == str:
+                    if v.startswith("0x"):
+                        record["winlog"]["event_data"][k] = int(v, 16)
+                    else:
+                        try:
+                            record["winlog"]["event_data"][k] = int(v)
+                        except ValueError:
+                            record["winlog"]["event_data"][k] = 0
 
-            # Move event attributes to ECS location
-            record["winlog"]["event_data"] = record["data"]["Event"].get(
-                "EventData", dict()
-            )
-            del record["data"]
-            if (
-                record["winlog"]["event_data"] is None
-                or len(record["winlog"]["event_data"]) == 0
-            ):  # remove event_data fields if empty
-                del record["winlog"]["event_data"]
-            else:
-                for k, v in record["winlog"]["event_data"].items():
-                    # Normalize some known problematic fields with values switching between integers and strings with hexadecimal notation to integers
-                    if k in ("ProcessId") and type(v) == str:
-                        if v.startswith("0x"):
-                            record["winlog"]["event_data"][k] = int(v, 16)
-                        else:
-                            try:
-                                record["winlog"]["event_data"][k] = int(v)
-                            except ValueError:
-                                record["winlog"]["event_data"][k] = 0
+                # Maximum limit of numeric values in Elasticsearch
+                if type(v) is int:
+                    if v < -(2 ** 63):
+                        record["winlog"]["event_data"][k] = -(2 ** 63)
+                    elif v > 2 ** 63 - 1:
+                        record["winlog"]["event_data"][k] = 2 ** 63 - 1
 
-                    # Maximum limit of numeric values in Elasticsearch
-                    if type(v) is int:
-                        if v < -(2 ** 63):
-                            record["winlog"]["event_data"][k] = -(2 ** 63)
-                        elif v > 2 ** 63 - 1:
-                            record["winlog"]["event_data"][k] = 2 ** 63 - 1
+        return record
 
-            buffer.append(record)
+    def gen_records(self, size: int) -> Generator:
+        """A generator that reads records from an Evtx file and generates a dict for each record.
+
+        Args:
+            size (int): Buffer size.
+
+        Yields:
+            Generator: Yields List[dict].
+        """
+
+        buffer: List[dict] = list()
+
+        # generates records
+        for record in self.parser.records_json():
+
+            formatted_record: dict = self.format_record(record)
+            buffer.append(formatted_record)
 
             if len(buffer) >= size:
                 yield buffer
@@ -136,21 +175,45 @@ def evtx2es(
     host: str = "localhost",
     port: int = 9200,
     index: str = "evtx2es",
-    type: str = "evtx2es",
     size: int = 500,
     scheme: str = "http",
 ):
+    """Fast import of Windows EventLogs(.evtx) into Elasticsearch.
+
+    Args:
+        filepath (str):
+            Windows EventLogs to import into Elasticsearch.
+
+        host (str, optional):
+            Elasticsearch host address. Defaults to "localhost".
+
+        port (int, optional):
+            Elasticsearch port number. Defaults to 9200.
+
+        index (str, optional):
+            Name of the index to create. Defaults to "evtx2es".
+
+        size (int, optional):
+            Buffer size for BulkIndice at a time. Defaults to 500.
+
+        scheme (str, optional):
+            Elasticsearch address scheme. Defaults to "http".
+    """
     es = ElasticsearchUtils(hostname=host, port=port, scheme=scheme)
     r = Evtx2es(filepath)
 
-    for records in tqdm(r.gen_json(size)):
+    for records in tqdm(r.gen_records(size)):
         try:
-            es.bulk_indice(records, index, type)
+            es.bulk_indice(records, index)
         except Exception:
             traceback.print_exc()
 
 
 def main():
+    """ This function is loaded when used from the console.
+    """
+
+    # Args
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "evtxfiles",
@@ -158,16 +221,16 @@ def main():
         type=Path,
         help="Windows EVTX files or directories containing them.",
     )
-    parser.add_argument(
-        "--host", default="localhost", help="ElasticSearch host address"
-    )
-    parser.add_argument("--port", default=9200, help="ElasticSearch port number")
+
+    # Options
+    parser.add_argument("--host", default="localhost", help="Elasticsearch host")
+    parser.add_argument("--port", default=9200, help="Elasticsearch port number")
     parser.add_argument("--index", default="evtx2es", help="Index name")
-    parser.add_argument("--type", default="evtx2es", help="Document type name")
     parser.add_argument("--size", default=500, help="Bulk insert buffer size")
     parser.add_argument("--scheme", default="http", help="Scheme to use (http, https)")
     args = parser.parse_args()
 
+    # Target files
     evtxfiles = list()
     for evtxfile in args.evtxfiles:
         if evtxfile.is_dir():
@@ -176,17 +239,20 @@ def main():
         else:
             evtxfiles.append(evtxfile)
 
+    # Indexing evtx files
     for evtxfile in evtxfiles:
-        print(f"Importing {evtxfile}")
+        print(f"Currently Importing {evtxfile}")
         evtx2es(
             filepath=evtxfile,
             host=args.host,
             port=int(args.port),
             index=args.index,
-            type=args.type,
             size=int(args.size),
             scheme=args.scheme,
         )
+        print()
+
+    print("Import completed.")
 
 
 if __name__ == "__main__":
